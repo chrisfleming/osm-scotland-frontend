c0483a9bf50cfec7b955d4bc90d9482a44876fba
Chris Fleming <me@chrisfleming.org>
2012-11-19 21:12:09 +0000

    Created initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OsmScotlandFrontend::Application.config.secret_token = '5c812d14198452cbcb681c8c236c4f4f72215ea752b12a3148a929f253ecaf69d081b27c637102656381d277a5ce69111c5edee69ed6b00df7331f7a951d7ae4'
